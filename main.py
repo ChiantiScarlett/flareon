0792f5de8ba1287503d5e96aa156c50fa4a94c09
Chianti Shiina <chianti.shiina@gmail.com>
2018-07-30 06:59:47 +0900

    created server-side, hooked up to Dropbox API

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from media_sync import FlareonMediaManager
+from werkzeug.utils import secure_filename
+from pprint import pprint
 app = Flask(__name__)
+
+manager = FlareonMediaManager()
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/media/add', methods=['POST'])
+def media_add():
+    if request.method == 'POST':
+        media_file = request.files['file']
+
+        filename = secure_filename(media_file.filename)
+        fp = media_file.stream
+        manager.add_media(filename, fp)
+
+        return render_template("media.html", media=manager.media)
+
+
+@app.route('/media/remove', methods=['POST'])
+def media_remove():
+    if request.method == 'POST':
+        local_media_id = request.form['media_id']
+        manager.remove_media(local_media_id)
+        return render_template("media.html", media=manager.media)
 
 
 if __name__ == "__main__":
--- static/flareon.js
+++ static/flareon.js
@@ -3,13 +3,48 @@ function update_weekday()
     key = ["Sunday", "Monday", "Tuesday", "Wednesday",
            "Thursday", "Friday", "Saturday"];
 
-    weekday = new Date(
-        $('#year-value').val(),
-        $('#month-value').val(),
-        $('#day-value').val()).getDay();
-    $('#weekday-value').text("("+key[weekday]+")");
+    date = new Date();
+    $('#year-value').val(date.getFullYear());
+    $('#month-value').val(date.getMonth()+1);
+    $('#day-value').val(date.getDate());
+    
+    $('#weekday-value').text("("+key[date.getDay()]+")");
 }
 
+function add_media()
+{
+    var form_data = new FormData($('#media-upload')[0]);
+    $.ajax({
+        type: "POST",
+        url: '/media/add',
+        data: form_data,
+        contentType: false,
+        cache: false,
+        processData: false,
+        success: function(data)
+        {
+            $('#media-holder').html(data);
+        },
+        failure: function(e)
+        {
+            alert('failed');
+        }
+    });
+
+}
+
+function remove_media(media_id)
+{
+    $.ajax({
+        type: "POST",
+        url: '/media/remove',
+        data: {"media_id":media_id},
+        success: function(data)
+        {
+            $('#media-holder').html(data);
+        }
+    })
+}
 
 $(document).ready(function()
 {
@@ -18,5 +53,16 @@ $(document).ready(function()
          element: $("#Editor")[0]
     });
 
-    $('#year-value').change(function(){update_weekday()});
+    update_weekday();
+
+    /* Media Management */
+    $("#add-media").click(function()
+    {
+
+        $("#fileholder").trigger("click");
+    });
+    $("#fileholder").change(function()
+    {
+        add_media();
+    });
 });
